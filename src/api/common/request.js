9bde77b5ebab0e4ef1afaa65a0e556405cf7c6cc
lucong <332553882@qq.com>
2018-12-12 14:34:39 +0800

    ad

--- src/api/common/request.js
+++ src/api/common/request.js
@@ -28,8 +28,9 @@ class Request {
       let ret = res.body
       console.log(ret)
       if (ret.code == -100 || ret.code == -101) {
-        this.dataHref('/auth/err?code=' + ret.code)
-        return
+        // this.dataHref('/auth/err?code=' + ret.code)
+        this.jump(ret.code)
+        // return
       }
       return res.body
     } else {
@@ -39,9 +40,28 @@ class Request {
 
   }
 
-  dataHref(url) {
-    document.getElementById("href-true").setAttribute("href", url); //传入参数  
-    document.getElementById("href-true").click(); //模拟点击  
+  jump(code) {
+
+    let html = `<div class="page bg-white">
+    <div class="p-5 text-center">
+      <span></span>`
+    if (code == -100) {
+      html += `<div>
+          <span class="text-muted">授权失败</span>
+        </div>
+        <div>
+          <hr>
+          <small class="text-muted h1">请返回重新登录</small>
+        </div>`
+    } else if (code - 101) {
+      html += `<p class="text-muted">您的账号还未通过审核，请耐心等待！</p>
+        <hr>
+        <p>若还未上传实名认证资料，请到 [我的] - [实名认证] 中上传真实资料信息</p>`
+    }
+    html += `</div>
+      </div>`
+
+    document.getElementsByTagName('body')[0].innerHTML = html
   }
 
 
