a3c13525336712decd9d9463200c15881c97b252
lucong <332553882@qq.com>
2018-12-12 15:47:28 +0800

    ad

---
+++ src/pages/invite/download.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="page">
+    <div class="mt-5 p-5 text-center">
+      <a href class="btn btn-lg btn-primary btn-block btn-radius-big">点击下载</a>
+      <hr>
+      <small class="text-muted">请在手机默认浏览器打开</small>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
