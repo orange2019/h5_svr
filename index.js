4adb9e39a2f4bba9d4948d79335753a29d4be206
lucong <332553882@qq.com>
2018-12-24 19:41:09 +0800

    ad

--- index.js
+++ index.js
@@ -21,21 +21,25 @@ const Log = require('./log');
 const bodyParser = require('body-parser'); // 处理请求中body的内容
 const methodOverride = require('method-override');
 
-// const session = require('express-session'); // session中间件
-// const RedisStrore = require('connect-redis')(session);
+// const session = require("express-session"); // session中间件
+// const RedisStrore = require("connect-redis")(session);
+
 // const sessionStore = {
-//   url: 'redis://orangecache.j7zctr.ng.0001.use2.cache.amazonaws.com:6379'
-// }
-// session 支持
+//   host: process.env.NODE_ENV == "production" ? "127.0.0.1" : "47.52.193.103",
+//   // host: 'ec2-54-169-177-12.ap-southeast-1.compute.amazonaws.com',
+//   port: 6379
+// };
+// // session 支持
 // app.use(
 //   session({
+//     // name: 'connect.sid',
 //     resave: false,
 //     saveUninitialized: true,
 //     cookie: {
 //       secure: false,
-//       maxAge: 1800000
+//       maxAge: 1000 * 60 * 60 * 24 * 7
 //     },
-//     secret: '123456', // session加密
+//     secret: "123456", // session加密
 //     store: new RedisStrore(sessionStore)
 //   })
 // );
