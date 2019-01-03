51e2b05fe8941c7810ed1214769cfbda6a7949aa
lucong <332553882@qq.com>
2019-01-03 13:13:40 +0800

    da

--- src/pages/auth/resetMobile.vue
+++ src/pages/auth/resetMobile.vue
@@ -2,10 +2,7 @@
   <div class="page">
     <div class="page-top page-new-list-top row bg-primary">
       <div class="col-2 pl-0">
-        <a href="javascript:window.android.close();" class="pl-3 d-block" v-if="isAndroid">
-          <span class="fas fa-chevron-left text-white"></span>
-        </a>
-        <a href="javascript:history.go(-1);" class="pl-3 d-block" v-else>
+        <a href="javascript:window.android.close();" class="pl-3 d-block">
           <span class="fas fa-chevron-left text-white"></span>
         </a>
       </div>
@@ -88,9 +85,6 @@ export default {
   computed: {
     token() {
       return this.$route.query.token;
-    },
-    isAndroid() {
-      return window.hasOwnProperty("android") ? true : false;
     }
   },
   methods: {
